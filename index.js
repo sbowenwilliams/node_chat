723eb5a54a5e050a01ed98fd31feeaebc24fdb51
Sean Bowen-Williams <sean.bowen-williams@yello.co>
2016-05-01 10:49:38 -0500

    testing differen port

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var express = require("express");
-var app = express(), http = require('http'), server = http.createServer(app), io = require('socket.io').listen(server);
-var sockets = io;
-var port=Number(process.env.PORT || 3000);
+var app = express();
+
+var port=Number(process.env.PORT || 5000);
 
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
-app.get("/", function(req, res){
-      res.render("page");
-});
 app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req, res){
+  res.render("page");
+});
+
+var io = require('socket.io').listen(app.listen(port));
 
 io.sockets.on('connection', function (socket) {
   socket.emit('message', { message: 'welcome to the chat' });
@@ -17,5 +20,4 @@ io.sockets.on('connection', function (socket) {
     io.sockets.emit('message', data);
   });
 });
-
 console.log("Listening on port " + port);
